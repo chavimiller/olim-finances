dcb98172c8ba978ced69e09ca2e654e3c285b8f2
chavimiller <chaviweisman@gmail.com>
2026-01-27 16:05:27 +0200

    create config file for google passport authentication, add client id and client secret to env file

--- api/routes/authRouter.js
+++ api/routes/authRouter.js
@@ -5,4 +5,18 @@ const validateUser = require("../validators/signUpValidator");
 
 authRouter.post("/signup", validateUser, authController.signUpPost);
 
+authRouter.get(
+  "/google",
+  passport.authenticate("google", { scope: ["profile"] }),
+);
+
+authRouter.get(
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/");
+  },
+);
+
 module.exports = authRouter;
