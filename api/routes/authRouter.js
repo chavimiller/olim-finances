e6c3fd265dc6701b6e497ec8e6b9f1c7d5ec5a57
chavimiller <chaviweisman@gmail.com>
2026-01-27 15:17:31 +0200

    add controller and data validator to signup router path

--- api/routes/authRouter.js
+++ api/routes/authRouter.js
@@ -1,1 +1,8 @@
-const authController = require("../controllers/authControllers");
+const { Router } = require("express");
+const authRouter = Router();
+const authController = require("../controllers/authController");
+const validateUser = require("../validators/signUpValidator");
+
+authRouter.post("/signup", validateUser, authController.signUpPost);
+
+module.exports = authRouter;
