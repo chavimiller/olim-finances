187e06ab34c4d88049d89d304ce7f0bd22ee5807
chavimiller <chaviweisman@gmail.com>
2026-01-27 14:53:02 +0200

    create validator for user signup form

---
+++ api/validators/signUpValidator.js
@@ -0,0 +1,25 @@
+const { body } = require("express-validator");
+
+const validateUser = [
+  body("email")
+    .trim()
+    .normalizeEmail()
+    .notEmpty()
+    .withMessage("Email is a required field")
+    .isEmail()
+    .withMessage("Must be a valid email."),
+
+  body("name")
+    .trim()
+    .escape()
+    .notEmpty()
+    .withMessage("Name is a required field.")
+    .isLength({ min: 1, max: 50 })
+    .withMessage("Name must be between 1 and 50` characters"),
+
+  body("password")
+    .notEmpty()
+    .withMessage("Password is a required field.")
+    .isLength({ min: 10 })
+    .withMessage("Password must be at least 10 characters."),
+];
