9dc5110600848e94376a36c6242f7e031b49e7a5
chavimiller <chaviweisman@gmail.com>
2026-03-17 15:52:25 +0200

    Complete basic login and signin pages and forms

--- user-client/src/components/Login.jsx
+++ user-client/src/components/Login.jsx
@@ -3,11 +3,86 @@ import { useState } from "react";
 const Login = () => {
   const [signIn, setSignIn] = useState(true);
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+
+    const endpoint = signIn ? "/auth/login" : "/auth/signup";
+
+    const body = {
+      email: formData.get("email"),
+      password: formData.get("password"),
+    };
+    if (!signIn) {
+      body.confirmPass = formData.get("confirmPass");
+    }
+    const res = await fetch(endpoint, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+  };
+
   if (signIn) {
     return (
       <div className="mainContainer">
         <div className="containerA">
-          <h1 className="signInTitle">Sign in</h1>
+          <h1 className="signInTitle">Login</h1>
+
+          <svg className="googleIcon" viewBox="0 0 128 128">
+            <path
+              fill="#fff"
+              d="M44.59 4.21a63.28 63.28 0 004.33 120.9 67.6 67.6 0 0032.36.35 57.13 57.13 0 0025.9-13.46 57.44 57.44 0 0016-26.26 74.33 74.33 0 001.61-33.58H65.27v24.69h34.47a29.72 29.72 0 01-12.66 19.52 36.16 36.16 0 01-13.93 5.5 41.29 41.29 0 01-15.1 0A37.16 37.16 0 0144 95.74a39.3 39.3 0 01-14.5-19.42 38.31 38.31 0 010-24.63 39.25 39.25 0 019.18-14.91A37.17 37.17 0 0176.13 27a34.28 34.28 0 0113.64 8q5.83-5.8 11.64-11.63c2-2.09 4.18-4.08 6.15-6.22A61.22 61.22 0 0087.2 4.59a64 64 0 00-42.61-.38z"
+            ></path>
+            <path
+              fill="#e33629"
+              d="M44.59 4.21a64 64 0 0142.61.37 61.22 61.22 0 0120.35 12.62c-2 2.14-4.11 4.14-6.15 6.22Q95.58 29.23 89.77 35a34.28 34.28 0 00-13.64-8 37.17 37.17 0 00-37.46 9.74 39.25 39.25 0 00-9.18 14.91L8.76 35.6A63.53 63.53 0 0144.59 4.21z"
+            ></path>
+            <path
+              fill="#f8bd00"
+              d="M3.26 51.5a62.93 62.93 0 015.5-15.9l20.73 16.09a38.31 38.31 0 000 24.63q-10.36 8-20.73 16.08a63.33 63.33 0 01-5.5-40.9z"
+            ></path>
+            <path
+              fill="#587dbd"
+              d="M65.27 52.15h59.52a74.33 74.33 0 01-1.61 33.58 57.44 57.44 0 01-16 26.26c-6.69-5.22-13.41-10.4-20.1-15.62a29.72 29.72 0 0012.66-19.54H65.27c-.01-8.22 0-16.45 0-24.68z"
+            ></path>
+            <path
+              fill="#319f43"
+              d="M8.75 92.4q10.37-8 20.73-16.08A39.3 39.3 0 0044 95.74a37.16 37.16 0 0014.08 6.08 41.29 41.29 0 0015.1 0 36.16 36.16 0 0013.93-5.5c6.69 5.22 13.41 10.4 20.1 15.62a57.13 57.13 0 01-25.9 13.47 67.6 67.6 0 01-32.36-.35 63 63 0 01-23-11.59A63.73 63.73 0 018.75 92.4z"
+            ></path>
+          </svg>
+          <h2>Or login with email</h2>
+          <form onSubmit={handleSubmit}>
+            <input type="text" name="email" id="email" placeholder="Email" />
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Password"
+              required
+            />
+
+            <div className="button2">
+              <button type="submit">Login</button>
+            </div>
+          </form>
+        </div>
+
+        <div className="containerB">
+          <h2 className="heading2">Don't have an account yet? </h2>
+          <div onClick={() => setSignIn(false)} className="button1">
+            <button>Join</button>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="mainContainer">
+        <div className="containerA">
+          <h1 className="signInTitle">Sign up</h1>
 
           <svg className="googleIcon" viewBox="0 0 128 128">
             <path
@@ -32,30 +107,38 @@ const Login = () => {
             ></path>
           </svg>
           <h2>Or create an account with an email</h2>
-          <form action="post">
-            <input type="text" name="email" id="email" placeholder="Email" />
+          <form onSubmit={handleSubmit}>
             <input
               type="text"
+              name="email"
+              id="email"
+              placeholder="Email"
+              required
+            />
+            <input
+              type="password"
               name="password"
               id="password"
               placeholder="Password"
+              required
             />
             <input
-              type="text"
+              type="password"
               name="confirmPass"
               id="confirmPass"
               placeholder="Confirm password"
+              required
             />
             <div className="button2">
-              <button>Login</button>
+              <button type="submit">Create account</button>
             </div>
           </form>
         </div>
 
         <div className="containerB">
-          <h2 className="heading2">Don't have an account yet? </h2>
-          <div className="button1">
-            <button>Join today</button>
+          <h2 className="heading2">Already have an account?</h2>
+          <div onClick={() => setSignIn(true)} className="button1">
+            <button>Sign in</button>
           </div>
         </div>
       </div>
