9f11bd699e21731aaade08e668703855e3e8f307
chavimiller <chaviweisman@gmail.com>
2026-03-17 16:19:52 +0200

    Create investment form file

--- user-client/src/App.jsx
+++ user-client/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Login from "./components/Login";
+import InvestmentForm from "./components/InvestmentForm";
 
 function App() {
   return (
     <>
       <Login />
+      <InvestmentForm />
     </>
   );
 }
